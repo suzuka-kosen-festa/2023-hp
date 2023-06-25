a5672bd184e2312a2b21e0490eec0aebc12ab66f
Rintaro Itokawa <me@re-taro.dev>
2023-06-26 00:47:23 +0900

    feat: implement top page

--- src/pages/index/index.page.tsx
+++ src/pages/index/index.page.tsx
@@ -1,16 +1,3 @@
-import { Counter } from "./Counter";
+import { Hero } from "~/domain";
 
-export const Page = () => {
-  return (
-    <>
-      <h1>Welcome</h1>
-      This page is:
-      <ul>
-        <li>Rendered to HTML.</li>
-        <li>
-          Interactive. <Counter />
-        </li>
-      </ul>
-    </>
-  );
-};
+export const Page = () => <Hero soon />;
