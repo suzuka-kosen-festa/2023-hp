4e39087582f41e17c2ecb8c483c2421de9c995c5
Rintaro Itokawa <me@re-taro.dev>
2023-06-26 01:00:03 +0900

    fix: fix viewport

--- src/renderer/_default.page.server.tsx
+++ src/renderer/_default.page.server.tsx
@@ -19,6 +19,9 @@ export const render = async (pageContext: PageContextServer) => {
   const documentHtml = escapeInject`<!DOCTYPE html>
     <html>
       <head>
+        <meta charset="utf8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <title>${title}</title>
       </head>
       <body>
